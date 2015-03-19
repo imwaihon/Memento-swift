02d748eb95f929432e6f8e604f6be79667167525
zixian92 <zixian1992@hotmail.com>
2015-03-19 17:04:09 +0800

    Adds MementoNode class

---
+++ Memento/Memento/MementoNode.swift
@@ -0,0 +1,18 @@
+//
+//  MementoNode.swift
+//  Memento
+//
+//  Represents a node in the memory palace.
+//
+//  Specifications:
+//
+//
+//  Created by Qua Zi Xian on 19/3/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
+
+class MementoNode {
+
+}
