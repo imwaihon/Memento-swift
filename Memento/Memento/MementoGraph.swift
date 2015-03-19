85d54212b5917062e7353453e8c73ef3e3756c94
zixian92 <zixian1992@hotmail.com>
2015-03-19 17:01:05 +0800

    Adds specifications to MementoGraph class

---
+++ Memento/Memento/MementoGraph.swift
@@ -0,0 +1,26 @@
+//
+//  MementoGraph.swift
+//  Memento
+//
+//  Represents a graph object in the application.
+//
+//  Specifications:
+//  Able to represent multigraphs.
+//  Easily identify and access to all nodes in the graph.
+//  Identifying/Enumerating distinct neighbours of any node.
+//  Insert/Remove nodes and edges without having to relabel nodes.
+//  Generate a level graph using Breadth-First Search.
+//
+//  Non-functional requirements:
+//  Acceptably low latency for all methods.
+//  Compact node labelling range.
+//
+//  Created by Qua Zi Xian on 19/3/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
+
+class MementoGraph {
+
+}
