a060f74375e9e763d078217d39a03061a45f09b1
zixian92 <zixian1992@hotmail.com>
2015-03-20 12:58:26 +0800

    Updates MementoGraph specifications

--- Memento/Memento/MementoGraph.swift
+++ Memento/Memento/MementoGraph.swift
@@ -15,6 +15,9 @@
 //  Acceptably low latency for all methods.
 //  Compact node labelling range.
 //
+//  Representation Invariant(s)
+//  Graph should have at least 1 node.
+//
 //  Created by Qua Zi Xian on 19/3/15.
 //  Copyright (c) 2015 NUS CS3217. All rights reserved.
 //
