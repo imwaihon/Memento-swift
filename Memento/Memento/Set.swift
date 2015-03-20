b4b915667b0b3b8af18b0156da099578384dafb5
zixian92 <zixian1992@hotmail.com>
2015-03-20 13:47:20 +0800

    Adds specifications to binary search tree

--- Memento/Memento/Set.swift
+++ Memento/Memento/Set.swift
@@ -2,8 +2,25 @@
 //  Set.swift
 //  Memento
 //
+//  Represents the balanced Binary Search Tree data structure.
+//
+//  Functional Specifications
+//  Insert element
+//  Remove element
+//  Find an element
+//  Get smallest element
+//  Get largest element
+//
+//  Non-functional Specifications
+//  O(log n) time complexity for functional operations.
+//  No duplicate elements.
+//
 //  Created by Qua Zi Xian on 20/3/15.
 //  Copyright (c) 2015 NUS CS3217. All rights reserved.
 //
 
 import Foundation
+
+class Set<T: Comparable> {
+
+}
