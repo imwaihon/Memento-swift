4e61365ff022f541bd7c690f01abd1330d5a762f
zixian92 <zixian1992@hotmail.com>
2015-03-20 17:42:03 +0800

    Adds unit tests for Set

---
+++ Memento/MementoTests/SetTests.swift
@@ -0,0 +1,65 @@
+//
+//  SetTests.swift
+//  Memento
+//
+//  Created by Qua Zi Xian on 20/3/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class SetTests: XCTestCase {
+    func testConstructor() {
+        let set = Set<Int>()
+        XCTAssertTrue(set.isEmpty)
+    }
+    
+    func testInsert() {
+        let set = Set<Int>()
+        for num in 1...7 {
+            set.insert(num)
+        }
+        for num in 1...7 {
+            XCTAssertTrue(set.contains(num))
+        }
+    }
+    
+    func testSmallestElement() {
+        let set = Set<Int>()
+        XCTAssertTrue(set.smallestElement == nil)
+        
+        for num in 1...7 {
+            set.insert(num)
+        }
+        XCTAssertEqual(set.smallestElement!, 1)
+    }
+    
+    func testLargestElement() {
+        let set = Set<Int>()
+        XCTAssertTrue(set.largestElement == nil)
+        
+        for num in 1...7 {
+            set.insert(num)
+        }
+        XCTAssertEqual(set.largestElement!, 7)
+    }
+    
+    func testErase() {
+        let set = Set<Int>()
+        for num in 1...7 {
+            set.insert(num)
+        }
+        
+        set.erase(5)
+        XCTAssertFalse(set.contains(5))
+        
+        set.erase(1)
+        XCTAssertFalse(set.contains(1))
+        XCTAssertEqual(set.smallestElement!, 2)
+        
+        set.erase(7)
+        XCTAssertFalse(set.contains(7))
+        XCTAssertEqual(set.largestElement!, 6)
+    }
+}
