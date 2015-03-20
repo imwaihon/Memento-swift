eae48437e2d93bfbe1297b23a42fb1e9297e3ed0
zixian92 <zixian1992@hotmail.com>
2015-03-20 13:33:25 +0800

    Adds specifications to MementoNode

--- Memento/Memento/MementoNode.swift
+++ Memento/Memento/MementoNode.swift
@@ -5,7 +5,13 @@
 //  Represents a node in the memory palace.
 //
 //  Specifications:
+//  Contains necessary information for display.
+//  Changes that will affect the displayed information should update the appropriate view components.
+//  Contains associations and links created by user.
+//  Identify association/link selected by user.
 //
+//  Non-functional specifications
+//  Easily identified by the graph containing it.
 //
 //  Created by Qua Zi Xian on 19/3/15.
 //  Copyright (c) 2015 NUS CS3217. All rights reserved.
---
+++ Memento/Memento/Set.swift
@@ -0,0 +1,9 @@
+//
+//  Set.swift
+//  Memento
+//
+//  Created by Qua Zi Xian on 20/3/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
