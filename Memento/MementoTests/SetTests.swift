4c8f4489a5f88386dad263949823ecff3b21a372
zixian92 <zixian1992@hotmail.com>
2015-03-21 12:44:19 +0800

    Makes removal test more comprehensive

--- Memento/MementoTests/SetTests.swift
+++ Memento/MementoTests/SetTests.swift
@@ -17,13 +17,14 @@ class SetTests: XCTestCase {
     
     func testInsert() {
         let set = Set<Int>()
+        let values = [7, 10, 13, 17, 15, 5, 4, 14, 16, 2, 6, 3, 1] //These values cause all possible balancing rotations
         for num in 1...7 {
             set.insert(num)
+            XCTAssertEqual(set.size, num)
         }
         for num in 1...7 {
             XCTAssertTrue(set.contains(num))
         }
-        XCTAssertEqual(set.size, 7)
     }
     
     func testSmallestElement() {
@@ -48,30 +49,74 @@ class SetTests: XCTestCase {
     
     func testErase() {
         let set = Set<Int>()
-        for num in 1...7 {
-            set.insert(num)
+        let values = [7, 10, 13, 17, 15, 5, 4, 14, 16, 2, 6, 3, 1, 15, 8, 9]
+        
+        for value in values {
+            set.insert(value)
         }
+        XCTAssertEqual(set.size, 15)
         
+        //Tests removal as well as handling of duplicates in insertion.
+        set.erase(15)
+        XCTAssertFalse(set.contains(15))
+        XCTAssertEqual(set.size, 14)
+        
+        //Tests removal of element with 2 children, causing rotation at successor's parent.
+        //Rotation/Balancing is only for efficiency but is complicated.
+        //Hard to test for correctness except by testing for regressions caused by balancing.
         set.erase(5)
+        XCTAssertEqual(set.size, 13)
         XCTAssertFalse(set.contains(5))
-        XCTAssertEqual(set.size, 6)
         
-        set.erase(1)
-        XCTAssertFalse(set.contains(1))
-        XCTAssertEqual(set.size, 5)
-        XCTAssertEqual(set.smallestElement!, 2)
+        //Tests removal of non-existent element
+        set.erase(15)
+        XCTAssertEqual(set.size, 13)
         
-        set.erase(7)
-        XCTAssertFalse(set.contains(7))
-        XCTAssertEqual(set.size, 4)
-        XCTAssertEqual(set.largestElement!, 6)
-        
+        //Tests removal of element with only left child
         set.erase(2)
-        XCTAssertEqual(set.size, 3)
+        XCTAssertEqual(set.size, 12)
+        XCTAssertFalse(set.contains(2))
+        
+        //Tests removal of element with only left child
+        set.erase(13)
+        XCTAssertEqual(set.size, 11)
+        XCTAssertFalse(set.contains(13))
+        
+        //Tests removal of smallest element and leaf.
+        set.erase(1)
+        XCTAssertEqual(set.size, 10)
+        XCTAssertFalse(set.contains(1))
+        XCTAssertEqual(set.smallestElement!, 3)
+        
+        //Removal of element with 2 children, with the successor being the right child.
+        set.erase(8)
+        set.erase(9)
+        
+        //This causes left rotation about 3, followed by right rotation about 6.
+        set.erase(7)
+        XCTAssertEqual(set.size, 7)
+        
+        //Removal of root
+        set.erase(10)
+        XCTAssertEqual(set.size, 6)
+        XCTAssertFalse(set.contains(10))
+        
+        //Removal of largest element
+        set.erase(17)
+        XCTAssertEqual(set.size, 5)
+        XCTAssertFalse(set.contains(17))
+        XCTAssertEqual(set.largestElement!, 16)
+        
+        //Cause right rotation about the root
+        set.erase(16)
+        
+        //Cause left rotation about root
         set.erase(3)
-        XCTAssertEqual(set.size, 2)
+        XCTAssertEqual(set.size, 3)
+        
+        //Removes the remaining elements
         set.erase(4)
-        XCTAssertEqual(set.size, 1)
+        set.erase(14)
         set.erase(6)
         XCTAssertTrue(set.isEmpty)
     }
