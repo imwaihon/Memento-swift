5f6c63955389734516810dcf2bb8dc3dd5481006
zixian92 <zixian1992@hotmail.com>
2015-03-23 20:02:04 +0800

    Implements a few basic methods in graph

--- Memento/Memento/MementoGraph.swift
+++ Memento/Memento/MementoGraph.swift
@@ -17,6 +17,7 @@
 //
 //  Representation Invariant(s)
 //  Graph should have at least 1 node.
+//  There should be at most 1 edge between any pair of nodes.
 //
 //  Created by Qua Zi Xian on 19/3/15.
 //  Copyright (c) 2015 NUS CS3217. All rights reserved.
@@ -25,5 +26,75 @@
 import Foundation
 
 class MementoGraph {
-
+    let name: String
+    private var adjList: [[Int]]
+    private var nodes: [MementoNode?]
+    private let nodeLabelGenerator: Set<Int>
+    //private var associations: [Association]
+    //private var links: [Link]
+    
+    init(name: String, root: MementoNode) {
+        self.name = name
+        adjList = [[Int]]()
+        nodes = [MementoNode?]()
+        //associations = [Association]()
+        //links = [Link]()
+        
+        nodeLabelGenerator = Set<Int>() //Initialise a small set of available labels
+        for idx in 0...10 {
+            nodeLabelGenerator.insert(idx)
+        }
+        
+        insertNode(root)
+    }
+    
+    private func insertNode(node: MementoNode){
+        node.label = nodeLabelGenerator.smallestElement!
+        nodeLabelGenerator.erase(node.label)
+        nodes[node.label] = node
+        adjList[node.label].removeAll(keepCapacity: false)
+        
+        //Replenish the label generator's labels
+        if nodeLabelGenerator.size < 3 {
+            let base = nodeLabelGenerator.largestElement!+1
+            for i in 0...9 {
+                nodeLabelGenerator.insert(base+i)
+            }
+        }
+    }
+    
+    private func insertEdge(source: MementoNode, destination: MementoNode){
+        let sLabel = source.label
+        let dLabel = destination.label
+        assert(sLabel>=0 && sLabel<nodes.count && nodes[sLabel] != nil)
+        assert(dLabel>=0 && dLabel<nodes.count && nodes[dLabel] != nil)
+        if !contains(adjList[sLabel], dLabel) {
+            adjList[sLabel].append(dLabel)
+        }
+        if !contains(adjList[dLabel], sLabel) {
+            adjList[dLabel].append(sLabel)
+        }
+    }
+    
+    //Gets the neighbours of the given node
+    //Returns an empty list if the given node does not exist or is invalid.
+    func getNeighbours(node: MementoNodeIcon) -> [MementoNodeIcon] {
+        let sLabel = node.label
+        let set = Set<Int>()
+        var res = [MementoNodeIcon]()
+        
+        if sLabel < 0 || sLabel >= nodes.count || nodes[sLabel] == nil {
+            return res
+        }
+        
+        let size = adjList[sLabel].count
+        for i in 0..<size {
+            let dLabel = adjList[sLabel][i]
+            if !set.contains(dLabel) {
+                assert(nodes[dLabel] != nil)
+                res.append(MementoNodeIcon(label: dLabel, filename: nodes[dLabel]!.imageFile))
+            }
+        }
+        return res
+    }
 }
