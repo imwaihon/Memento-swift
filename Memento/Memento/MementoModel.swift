fe48fa9d5fa2e86ecccf47da2e10515c0302818f
zixian92 <zixian1992@hotmail.com>
2015-03-25 16:03:42 +0800

    Adds the app's model class

---
+++ Memento/Memento/MementoModel.swift
@@ -0,0 +1,15 @@
+//
+//  MementoModel.swift
+//  Memento
+//
+//  The app model that stores teh runtime representations of the graphs and nodes.
+//
+//  Created by Qua Zi Xian on 25/3/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
+
+class MementoModel {
+
+}
