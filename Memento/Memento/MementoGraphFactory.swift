0b1406a3fbf6f7c38d8b56c3ceb4de990b412406
zixian92 <zixian1992@hotmail.com>
2015-03-25 16:13:40 +0800

    Adds factory classes

---
+++ Memento/Memento/MementoGraphFactory.swift
@@ -0,0 +1,20 @@
+//
+//  MementoGraphFactory.swift
+//  Memento
+//
+//  The factory object for the memory palace.
+//
+//  Specifications:
+//  Generate a memory palace object using the minimal information required to construct one that does not
+//  violate representation invariant.
+//  Construct a memory palace instance from a saved template.
+//
+//  Created by Qua Zi Xian on 25/3/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
+
+class MementoGraphFactory {
+
+}
---
+++ Memento/Memento/MementoNodeFactory.swift
@@ -0,0 +1,15 @@
+//
+//  MementoNodeFactory.swift
+//  Memento
+//
+//  The factory object for rooms in the memory palace.
+//
+//  Created by Qua Zi Xian on 25/3/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
+
+class MementoNodeFactory {
+    
+}
