b5073c16a8015335c65b1d9ed276992bf58da215
zixian92 <zixian1992@hotmail.com>
2015-03-27 11:06:55 +0800

    Adds constants file for the project

---
+++ Memento/Memento/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Memento
+//
+//  The file containing ALL constants used by the app.
+//
+//  Created by Qua Zi Xian on 27/3/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//Graph Icon Display Constants(modify as needed by UI developer)
+ let graphIconSize = CGSizeMake(100, 50)    //Tentative value
+
+//Graph Node Icon Display Constants(modify as needed by UI developer)
+let graphNodeIconSize = CGSizeMake(100, 50)
---
+++ Memento/Memento/MementoGraphIcon.swift
@@ -0,0 +1,9 @@
+//
+//  MementoGraphIcon.swift
+//  Memento
+//
+//  Created by Qua Zi Xian on 27/3/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
