03d1122dc12cf31f527867ca75cba7276fee1f62
zixian92 <zixian1992@hotmail.com>
2015-03-27 22:45:38 +0800

    Documents GraphIcon

--- Memento/Memento/MementoGraphIcon.swift
+++ Memento/Memento/MementoGraphIcon.swift
@@ -4,6 +4,10 @@
 //
 //  The icon representation of the graph in graph selection scene.
 //
+//  Specifications
+//  The view controller should be able to specify/identify the target memory
+//  palace when calling app manager methods.
+//
 //  Created by Qua Zi Xian on 27/3/15.
 //  Copyright (c) 2015 NUS CS3217. All rights reserved.
 //
@@ -11,11 +15,11 @@
 import Foundation
 
 struct MementoGraphIcon {
-    let graphName: String
+    let graphName: String       //The identifier for the graph
     let imageFile: String       //Used by view controller to load icon's image file
     
     init(graphName: String, imageFile: String){
-        graphNumber = graphName
+        self.graphName = graphName
         self.imageFile = imageFile
     }
 }
