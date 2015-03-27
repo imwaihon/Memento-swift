3af6819789826f88471bb1fa0e257c4ebac72dbd
zixian92 <zixian1992@hotmail.com>
2015-03-27 11:08:48 +0800

    Implements the graph ison data structure

--- Memento/Memento/MementoGraphIcon.swift
+++ Memento/Memento/MementoGraphIcon.swift
@@ -2,8 +2,20 @@
 //  MementoGraphIcon.swift
 //  Memento
 //
+//  The icon representation of the graph in graph selection scene.
+//
 //  Created by Qua Zi Xian on 27/3/15.
 //  Copyright (c) 2015 NUS CS3217. All rights reserved.
 //
 
 import Foundation
+
+struct MementoGraphIcon {
+    let graphNumber: Int        //Used as parameter when retrieving/removing graph from model
+    let imageFile: String       //Used by view controller to load icon's image file
+    
+    init(number: Int, imageFile: String){
+        graphNumber = number
+        self.imageFile = imageFile
+    }
+}
