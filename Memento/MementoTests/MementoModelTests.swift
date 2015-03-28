9af98c3e011cf2f975c4e9d0eb0838a2c573bddf
zixian92 <zixian1992@hotmail.com>
2015-03-28 09:51:25 +0800

    Adds unit test for model

---
+++ Memento/MementoTests/MementoModelTests.swift
@@ -0,0 +1,50 @@
+//
+//  MementoModelTests.swift
+//  Memento
+//
+//  Unit tests for model.
+//
+//  Created by Qua Zi Xian on 28/3/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class MementoModelTests: XCTestCase {
+    func testInit() {
+        let model = MementoModel()
+        XCTAssertEqual(model.numPalaces, 0)
+    }
+    
+    func testAddPalace() {
+        let model = MementoModel()
+        
+        model.addPalace(MementoGraph(name: "graph1", rootNode: MementoNode(imageFile: "A.png")))
+        XCTAssertEqual(model.numPalaces, 1)
+        XCTAssertFalse(model.getPalace("graph1") == nil)
+        
+        model.addPalace(MementoGraph(name: "graph2", rootNode: MementoNode(imageFile: "B.png")))
+        XCTAssertEqual(model.numPalaces, 2);
+        XCTAssertFalse(model.getPalace("graph2") == nil)
+    }
+    
+    func testRemovePalace() {
+        let model = MementoModel()
+        
+        model.addPalace(MementoGraph(name: "graph1", rootNode: MementoNode(imageFile: "A.png")))
+        model.addPalace(MementoGraph(name: "graph2", rootNode: MementoNode(imageFile: "B.png")))
+        XCTAssertEqual(model.numPalaces, 2)
+        XCTAssertFalse(model.getPalace("graph1") == nil)
+        XCTAssertFalse(model.getPalace("graph2") == nil)
+        
+        model.removePalace("graph1")
+        XCTAssertEqual(model.numPalaces, 1)
+        XCTAssertTrue(model.getPalace("graph1") == nil)
+        XCTAssertFalse(model.getPalace("graph2") == nil)
+        
+        model.removePalace("graph2")
+        XCTAssertEqual(model.numPalaces, 0)
+        XCTAssertTrue(model.getPalace("graph2") == nil)
+    }
+}
