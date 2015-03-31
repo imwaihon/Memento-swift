1b3a52973de34fe48157dbfd76ca05df0c771e72
imwaihon <imwaihon@gmail.com>
2015-03-31 21:53:24 +0800

    Implementation for Game Challenge Mode

---
+++ Memento/Memento/EndGameChallengeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EndGameChallengeViewController.swift
+//  Memento
+//
+//  Created by Chee Wai Hon on 31/3/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
