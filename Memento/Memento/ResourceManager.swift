aa9b9b5f16bf08ee9135b080062d7f7425b77d31
zixian92 <zixian1992@hotmail.com>
2015-04-09 13:04:04 +0800

    Implements basic retain and release methods

--- Memento/Memento/ResourceManager.swift
+++ Memento/Memento/ResourceManager.swift
@@ -12,16 +12,30 @@
 //
 
 import Foundation
+import UIKit
 
 class ResourceManager {
-    private let dirPath: String
+    private let _dirPath: String    //The base directory to read/write resource files.
+    private let _resourceListPath: String   //The full path of the resource tracking file.
     private var _referenceCountTable: [String: UInt]
-    private var saveQueue: dispatch_queue_t
+    private var saveQueue: dispatch_queue_t     //The queue used to dispatch all file save operations.
     
+    //Directory is the name of the directory inside the app's Document directory.
     init(directory: String) {
-        _referenceCountTable = [String: UInt]()
         saveQueue = dispatch_queue_create("com.cs3217.Memento.ResourceManager", DISPATCH_QUEUE_SERIAL)
-        dirPath = directory     //To be changed to the full path of the specified directory
+        
+        //Computing the path to save the resource tracking file
+        let docDir = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as String
+        _dirPath = docDir+"/\(directory)/"
+        _resourceListPath = docDir+"/\(_dirPath).plist"
+        
+        //Loads the reference counting table.
+        if NSFileManager.defaultManager().fileExistsAtPath(_resourceListPath) {
+            _referenceCountTable = NSDictionary(contentsOfFile: _resourceListPath) as [String: UInt]
+        } else {
+            _referenceCountTable = [String: UInt]()
+        }
+        //println(_resourceListPath)
     }
     
     //Increases reference count for the resource object identified by the given name.
@@ -29,13 +43,35 @@ class ResourceManager {
     func retainResource(resourceName: String) {
         if _referenceCountTable[resourceName] != nil {
             _referenceCountTable[resourceName]!++
+            /*dispatch_async(saveQueue, {() -> Void in
+                NSDictionary(dictionary: self._referenceCountTable).writeToFile(self._resourceListPath, atomically: true)
+            })*/
+            NSDictionary(dictionary: _referenceCountTable).writeToFile(self._resourceListPath, atomically: true)
         }
+    }
+    
+    //Saves the given image using the given name and sets reference count to 1.
+    //Overrides if there exists another file with the same name in the directory.
+    func retainResource(resourceName: String, image: UIImage) {
+        _referenceCountTable[resourceName] = 1
+        /*dispatch_async(saveQueue, {() -> Void in
+            UIImageJPEGRepresentation(image, 0.9).writeToFile(self._dirPath+resourceName, atomically: true)
+        })*/
+        UIImageJPEGRepresentation(image, 0.9).writeToFile(_dirPath+resourceName, atomically: true)
     }
     
     //Reduces the reference count for the resource object identified by the given name.
     //The resource object will be removed if there is no more reference to the resource after this operation.
     //Does nothing if the resource with the given name is not found.
     func releaseResource(resourceName: String) {
-        //To be implemented
+        if _referenceCountTable[resourceName] != nil {
+            if _referenceCountTable[resourceName] == 1 {
+                _referenceCountTable[resourceName] = nil
+                NSFileManager.defaultManager().removeItemAtPath(_dirPath+resourceName, error: nil)
+            } else {
+                _referenceCountTable[resourceName]!--
+            }
+            NSDictionary(dictionary: _referenceCountTable).writeToFile(_resourceListPath, atomically: true)
+        }
     }
 }
---
+++ Memento/MementoTests/ResourceManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ResourceManagerTests.swift
+//  Memento
+//
+//  Created by Qua Zi Xian on 9/4/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class ResourceManagerTests: XCTestCase {
+    func test() {
+        let manager = ResourceManager(directory: "sharedResource")
+    }
+}
