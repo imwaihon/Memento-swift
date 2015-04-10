bd325b99174613c64421965b56f6272fded08746
zixian92 <zixian1992@hotmail.com>
2015-04-10 10:10:16 +0800

    Completes test for resource manager on image resources

--- Memento/MementoTests/ResourceManagerTests.swift
+++ Memento/MementoTests/ResourceManagerTests.swift
@@ -2,6 +2,9 @@
 //  ResourceManagerTests.swift
 //  Memento
 //
+//  Currently testing on image resources only.
+//  The tests must run in the order: testAddAndRetainResource, testReleaseAndRemoveResource.
+//
 //  Created by Qua Zi Xian on 9/4/15.
 //  Copyright (c) 2015 NUS CS3217. All rights reserved.
 //
@@ -18,7 +21,6 @@ class ResourceManagerTests: XCTestCase {
     func testAddAndRetainResource() {
         let manager = ResourceManager(directory: "sharedResource")
         let imageFile = UIImage(named: NSBundle.mainBundle().pathForResource("linuxpenguin", ofType: ".jpg")!)
-        XCTAssertFalse(fileExists("linuxpenguin.jpg"))
         
         manager.retainResource("linuxpenguin.jpg", image: imageFile!)
         XCTAssertTrue(fileExists("sharedResource/linuxpenguin.jpg"))
@@ -33,8 +35,20 @@ class ResourceManagerTests: XCTestCase {
         XCTAssertEqual(manager.referenceCountForResource("B.png"), 0)
     }
     
-    func testRemoveResource() {
+    func testReleaseAndRemoveResource() {
+        let manager = ResourceManager(directory: "sharedResource")
+        XCTAssertTrue(fileExists("sharedResource/linuxpenguin.jpg"))
+        XCTAssertEqual(manager.referenceCountForResource("linuxpenguin.jpg"), 2)
         
+        //Check reference count decrementing.
+        manager.releaseResource("linuxpenguin.jpg")
+        XCTAssertTrue(fileExists("sharedResource/linuxPenguin.jpg"))
+        XCTAssertEqual(manager.referenceCountForResource("linuxpenguin.jpg"), 1)
+        
+        //Check resource deletion when reference count falls to 0.
+        manager.releaseResource("linuxpenguin.jpg")
+        XCTAssertFalse(fileExists("sharedResource/linuxpenguin.jpg"))
+        XCTAssertEqual(manager.referenceCountForResource("linuxpenguin.jpg"), 0)
     }
     
     private func fileExists(filename: String) -> Bool {
