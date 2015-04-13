4c58bf43e965d43a96769578272ea02e5952da77
Abdulla Contractor <abdulla.contractor@nus.edu.sg>
2015-04-13 20:52:25 +0800

    game menu changed to blur view

--- Memento/Memento/GameModePauseMenuViewController.swift
+++ Memento/Memento/GameModePauseMenuViewController.swift
@@ -1,49 +1,53 @@
 //
-//  GameMenuViewController.swift
+//  GameModePauseMenu.swift
 //  Memento
 //
-//  Created by Chee Wai Hon on 10/4/15.
+//  Created by Abdulla Contractor on 13/4/15.
 //  Copyright (c) 2015 NUS CS3217. All rights reserved.
 //
 
 import Foundation
 import UIKit
+import MobileCoreServices
+import QuartzCore
 
-class GameMenuViewController: UIViewController {
-    
-    
-    required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        
-    }
+class GameModePauseMenuViewController: UIViewController, UIGestureRecognizerDelegate,UINavigationControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = .clearColor()
         
         let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .Dark))
         visualEffectView.frame = self.view.bounds
         visualEffectView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
         visualEffectView.setTranslatesAutoresizingMaskIntoConstraints(true)
-    
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
+        tapGesture.numberOfTapsRequired = 1
+        visualEffectView.addGestureRecognizer(tapGesture)
+        
         self.view.addSubview(visualEffectView)
         self.view.sendSubviewToBack(visualEffectView)
-        self.setNeedsStatusBarAppearanceUpdate()
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
     
-
-    @IBAction func backButtonPress(sender: AnyObject) {
-        var controller = self.presentingViewController as? GameChallengeViewController
-        controller?.resumeGame()
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    func handleTap(sender: UITapGestureRecognizer){
         self.dismissViewControllerAnimated(true, completion: nil)
-        controller = nil
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.BlackOpaque
     }
     
     @IBAction func backToMenuButtonPress(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
         self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
-
     }
 }
