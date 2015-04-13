52cdb04ff77f17fd7d5c8df1759b1a92d362852c
Abdulla Contractor <abdulla.contractor@nus.edu.sg>
2015-04-13 21:40:45 +0800

    changed the challenge mode selection UI

--- Memento/Memento/SelectChallengeViewController.swift
+++ Memento/Memento/SelectChallengeViewController.swift
@@ -11,6 +11,15 @@ import UIKit
 
 class SelectChallengeViewController: UIViewController {
     
+    enum gameModes : String{
+        case orderMode = "Order"
+        case findMode = "Find"
+    }
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var modeTitleLabel: UILabel!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -19,9 +28,31 @@ class SelectChallengeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        playButton.layer.borderColor = UIColor.darkGrayColor().CGColor
+        playButton.layer.borderWidth = 1.5
+        playButton.layer.cornerRadius = 5
+        leftButton.hidden = true
+        rightButton.hidden = false
         
     }
-
+    
+    @IBAction func leftButtonClicked(sender: AnyObject) {
+        leftButton.hidden = true
+        rightButton.hidden = false
+        modeTitleLabel.text = gameModes.orderMode.rawValue
+    }
+    @IBAction func rightButtonClicked(sender: AnyObject) {
+        leftButton.hidden = false
+        rightButton.hidden = true
+        modeTitleLabel.text = gameModes.findMode.rawValue
+    }
+    @IBAction func playButtonPressed(sender: AnyObject) {
+        if(modeTitleLabel.text == gameModes.orderMode.rawValue){
+            self.performSegueWithIdentifier("loadOrderChallenge", sender: self)
+        } else if(modeTitleLabel.text == gameModes.findMode.rawValue){
+            
+        }
+    }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if(segue.identifier == "LoadCustomChallengeSegue"){
