ddb79574cdfa41e2231f1340e9d7ea603c2934c2
zixian92 <zixian1992@hotmail.com>
2015-04-19 23:41:40 +0800

    Adds description for association

--- Memento/Memento/Association.swift
+++ Memento/Memento/Association.swift
@@ -2,6 +2,8 @@
 //  Association.swift
 //  Memento
 //
+//  Defines an association object in memory palace room.
+//
 //  Created by Qua Zi Xian on 28/3/15.
 //  Copyright (c) 2015 NUS CS3217. All rights reserved.
 //
