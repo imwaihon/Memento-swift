207caf5d3cdefa2a868cb61985e4f85ca2fbc10b
zixian92 <zixian1992@hotmail.com>
2015-04-19 17:23:46 +0800

    Implements adding of text resource in resource manager

--- Memento/Memento/ResourceManager.swift
+++ Memento/Memento/ResourceManager.swift
@@ -7,6 +7,14 @@
 //  To achieve auto-reference counting(ARC) effect, wrap this in the class that is to provide ARC.
 //  The wrapper class will manually invoke the reference counting mechanism provided by this class.
 //
+//  Abstraction functions:
+//  Indicate use of resource:           retainResource(resourceName: String)
+//  Add text resource:                  retainResource(resourceName: String, text: String) -> String
+//  Add image resource:                 retainResource(resourceName: String, image: UIImage) -> String
+//  Indicate release of resource:       releaseResource(resourceName: String)
+//  Get reference count for resource:   referenceCountForResource(resourceName: String) -> Int
+//  Get list of resource:               resourceOfType(type: ResourceType) -> [String]
+//
 //  Usage Notes:
 //  This class treats the specified directory as an empty directory if the resource tracking file cannot be found.
 //  This class resets the tracking file if the specified directory cannot be found.
@@ -53,6 +61,7 @@ class ResourceManager {
         } else {
             _referenceCountTable = [String: UInt]()
             NSFileManager.defaultManager().createDirectoryAtPath(dir, withIntermediateDirectories: true, attributes: nil, error: nil)
+            NSDictionary(dictionary: _referenceCountTable).writeToFile(self._resourceListPath, atomically: true)
         }
     }
     
@@ -97,18 +106,39 @@ class ResourceManager {
     }
     
     //Saves the text resource to the specified file.
-    //If there exists a file with the same name, the existing file is overridden.
-    /*func retainResource(resourceName: String, text: String) {
-        _referenceCountTable[resourceName] = 1
-        text.writeToFile(_dirPath+resourceName, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
-    }*/
+    //Returns the actual name of the text file being saved to.
+    func retainResource(resourceName: String, text: String) -> String {
+        var filename = resourceName
+        
+        //Construct new file name if file with same name exists.
+        if _referenceCountTable[resourceName] != nil {
+            filename = filename.stringByDeletingPathExtension
+            for var i=1; ; i++ {
+                if _referenceCountTable[(filename+"(\(i))").stringByAppendingPathExtension(textExtension)!] == nil {
+                    filename+="(\(i))"
+                    break
+                }
+            }
+        }
+        
+        //Complete the file name and write to the file.
+        filename = filename.stringByAppendingPathExtension(textExtension)!
+        _referenceCountTable[filename] = 1
+        text.writeToFile(_dirPath+filename, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
+        
+        //Save the updated reference table
+        NSDictionary(dictionary: _referenceCountTable).writeToFile(self._resourceListPath, atomically: true)
+        return filename
+    }
     
     //Saves the given image using the given name and sets reference count to 1.
-    //Overrides if there exists another file with the same name in the directory.
+    //Returns the actual name of the image file used.
     //Resource name should have extensions .jpg or .png
     func retainResource(resourceName: String, image: UIImage) -> String {
         let ext = resourceName.pathExtension
         var filename = resourceName
+        
+        //Cinstruct new file name if file with same name exists.
         if _referenceCountTable[filename] != nil {
             filename = filename.stringByDeletingPathExtension
             for var i = 1; ; i++ {
@@ -119,12 +149,16 @@ class ResourceManager {
             }
             filename = filename.stringByAppendingPathExtension(ext)!
         }
+        
+        //Write to file.
         _referenceCountTable[filename] = 1
         if ext == jpegExtension {
             UIImageJPEGRepresentation(image, 0.9).writeToFile(_dirPath+filename, atomically: true)
         } else {
             UIImagePNGRepresentation(image).writeToFile(_dirPath+filename, atomically: true)
         }
+        
+        //Save the update reference table
         NSDictionary(dictionary: _referenceCountTable).writeToFile(self._resourceListPath, atomically: true)
         return filename
     }
