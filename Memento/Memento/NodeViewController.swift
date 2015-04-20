7ea746f1bafa34318f8a55a538be13efc62d8280
jjingrong <jing_rong92@hotmail.com>
2015-04-21 04:21:00 +0800

    Shows label number upon button press to implement for swapping later

--- Memento/Memento/NodeViewController.swift
+++ Memento/Memento/NodeViewController.swift
@@ -15,6 +15,7 @@ import QuartzCore
 
 class NodeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLImageEditorDelegate {
     
+    @IBOutlet weak var swapButton: UIButton!
     @IBOutlet weak var annotateWordButton: UIButton!
     @IBOutlet weak var downloadButton: UIButton!
     @IBOutlet weak var menuBarView: UIView!
@@ -26,20 +27,20 @@ class NodeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     var mementoManager = MementoManager.sharedInstance
     
     private var newImage: DraggableImageView!
-    private var lastRotation = CGFloat()
     private let panRec = UIPanGestureRecognizer()
     private var startPoint = CGPoint()
     private var allCGRects = [CGRect]()
+    private var allLabels = [UILabel]()
     
     var roomLabel = Int()
     var graphName = String()
     private var overlayList = [Overlay]()
     private var associationList = [Association]()
     
-    private var rotationToggler: Bool = true
     private var editToggler: Bool = true
     var deleteToggler: Bool = false
     private var annotateWordToggler: Bool = false
+    private var swappingToggler: Bool = false
     
     
     override func viewDidLoad() {
@@ -60,6 +61,8 @@ class NodeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         deleteModeButton.layer.borderColor = UIColor.clearColor().CGColor
         annotateWordButton.layer.borderWidth = 2.0
         annotateWordButton.layer.borderColor = UIColor.clearColor().CGColor
+        swapButton.layer.borderWidth = 2.0
+        swapButton.layer.borderColor = UIColor.clearColor().CGColor
         
         // Load
         overlayList = roomRep.overlays
@@ -99,125 +102,6 @@ class NodeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
             imageView.addSubview(newAnnotatableView)
         }
         
-    }
-    
-    // Toggles on edit mode
-    @IBAction func toggleEditMode(sender: AnyObject) {
-        
-        if self.editToggler == false {
-            self.editToggler = true
-            self.imageView.userInteractionEnabled = true
-            // Enable editing -> Show menu bar
-            UIView.animateWithDuration(NSTimeInterval(0.3), animations: {
-                var moveFrameUp = CGRectMake(0, self.view.frame.height - self.menuBarView.frame.height, self.menuBarView.frame.width, self.menuBarView.frame.height)
-                self.menuBarView.frame = moveFrameUp
-            })
-            
-        } else {
-            self.editToggler = false
-            self.imageView.userInteractionEnabled = false
-            // Disable editing -> Hide menu bar
-            UIView.animateWithDuration(NSTimeInterval(0.3), animations: {
-                var moveFrameDown = CGRectMake(0, self.view.frame.height, self.menuBarView.frame.width, self.menuBarView.frame.height)
-                self.menuBarView.frame = moveFrameDown
-            })
-        }
-        
-    }
-    
-    // Adds an overlaying image from camera roll ( possibly in-app sprites next time?)
-    @IBAction func addOverlayImage(sender: AnyObject){
-        if (editToggler == false) {
-            return
-        }
-        getImageFromPhotoLibrary(sender)
-        
-    }
-    
-    // Helper functions for main photo pickers
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-        
-        let mediaType = info[UIImagePickerControllerMediaType] as NSString
-        
-        self.dismissViewControllerAnimated(true, completion: nil)
-        
-        if mediaType.isEqualToString(kUTTypeImage as NSString) {
-            var image = info[UIImagePickerControllerOriginalImage]
-                as UIImage
-            
-            // Adjustments to show UIImageView in proper rotation according to input types
-            if picker.sourceType == UIImagePickerControllerSourceType.Camera{
-                image = UIImage(CGImage: image.CGImage, scale:1, orientation: UIImageOrientation.Down)!
-            } else {
-                image = UIImage(CGImage: image.CGImage, scale:1, orientation: UIImageOrientation.Up)!
-            }
-
-            // Adding draggable images
-            // Scaling factor for inserted images is default at height 150
-            var scalingFactor = image.size.height/150.0
-            var newWidth = image.size.width / scalingFactor
-            newImage = DraggableImageView(image: image)
-            newImage.graphName = self.graphName
-            newImage.roomLabel = self.roomLabel
-            newImage.frame = CGRect(x: imageView.center.x, y: imageView.center.y, width: newWidth, height: 150.0)
-            newImage.parentViewController = self
-            imageView.addSubview(newImage)
-            
-            // Get paths for saving
-            var newOverlay = mementoManager.addOverlay(graphName, roomLabel: roomLabel, frame: newImage.frame, image: (newImage.image!))!
-            newImage.labelIdentifier = newOverlay.label
-            
-            if (newMedia == true) {
-                // Option to save to camera roll/ Photo album
-                //UIImageWriteToSavedPhotosAlbum(image, self, "image:didFinishSavingWithError:contextInfo:", nil)
-            } else if mediaType.isEqualToString(kUTTypeMovie as NSString) {
-                // Code to support video here
-            }
-            
-        }
-    }
-    
-    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafePointer<Void>) {
-        
-        if error != nil {
-            let alert = UIAlertController(title: "Save Failed",
-                message: "Failed to save image",
-                preferredStyle: UIAlertControllerStyle.Alert)
-            
-            let cancelAction = UIAlertAction(title: "OK",
-                style: .Cancel, handler: nil)
-            
-            alert.addAction(cancelAction)
-            self.presentViewController(alert, animated: true,
-                completion: nil)
-        }
-    }
-    
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    @IBAction func dismissView(sender: UIButton) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-
-    private func getImageFromPhotoLibrary(sender: AnyObject) {
-        if UIImagePickerController.isSourceTypeAvailable(
-            UIImagePickerControllerSourceType.SavedPhotosAlbum) {
-                var imagePicker = UIImagePickerController()
-                imagePicker.delegate = self
-                imagePicker.sourceType =
-                    UIImagePickerControllerSourceType.PhotoLibrary
-                imagePicker.mediaTypes = [kUTTypeImage as NSString]
-                imagePicker.allowsEditing = false
-                
-                var popover = UIPopoverController(contentViewController: imagePicker) as UIPopoverController
-                var frame = CGRectMake(315, 260, 386, 386);
-                
-                popover.presentPopoverFromRect(frame, inView: self.view, permittedArrowDirections: UIPopoverArrowDirection.Any, animated: true)
-                
-                newMedia = false
-        }
     }
     
     // Method to draw rectangles for annotation
@@ -264,19 +148,31 @@ class NodeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         }
     }
     
-    func deleteView(view: UIView) {
-        if let indexToRect = find(allCGRects, view.frame) {
-            allCGRects.removeAtIndex(indexToRect)
-        }
-        view.removeFromSuperview()
-        
-        var roomRep = mementoManager.getMemoryPalaceRoomView(graphName, roomLabel: roomLabel)!
-        
-        overlayList = roomRep.overlays
-        associationList = roomRep.associations
-    }
-    
     /************************************** Menu buttons **************************************/
+    
+    // Toggles on edit mode
+    @IBAction func toggleEditMode(sender: AnyObject) {
+        
+        if self.editToggler == false {
+            self.editToggler = true
+            self.imageView.userInteractionEnabled = true
+            // Enable editing -> Show menu bar
+            UIView.animateWithDuration(NSTimeInterval(0.3), animations: {
+                var moveFrameUp = CGRectMake(0, self.view.frame.height - self.menuBarView.frame.height, self.menuBarView.frame.width, self.menuBarView.frame.height)
+                self.menuBarView.frame = moveFrameUp
+            })
+            
+        } else {
+            self.editToggler = false
+            self.imageView.userInteractionEnabled = false
+            // Disable editing -> Hide menu bar
+            UIView.animateWithDuration(NSTimeInterval(0.3), animations: {
+                var moveFrameDown = CGRectMake(0, self.view.frame.height, self.menuBarView.frame.width, self.menuBarView.frame.height)
+                self.menuBarView.frame = moveFrameDown
+            })
+        }
+        
+    }
     
     // Delete Mode Activated
     @IBAction func deleteButtonPressed(sender: UIButton) {
@@ -289,6 +185,19 @@ class NodeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
             deleteToggler = true
             deleteModeButton.layer.borderColor = UIColor.whiteColor().CGColor
         }
+    }
+
+    // Helper function to delete views
+    func deleteView(view: UIView) {
+        if let indexToRect = find(allCGRects, view.frame) {
+            allCGRects.removeAtIndex(indexToRect)
+        }
+        view.removeFromSuperview()
+        
+        var roomRep = mementoManager.getMemoryPalaceRoomView(graphName, roomLabel: roomLabel)!
+        
+        overlayList = roomRep.overlays
+        associationList = roomRep.associations
     }
     
     // Next Node button
@@ -322,16 +231,22 @@ class NodeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     // Save imageView to camera roll
     @IBAction func captureView(sender: AnyObject) {
-        UIGraphicsBeginImageContextWithOptions(self.imageView.bounds.size, view.opaque, 0.0)
-        self.imageView.layer.renderInContext(UIGraphicsGetCurrentContext())
-        var imageToCapture = UIGraphicsGetImageFromCurrentImageContext()
-        UIImageWriteToSavedPhotosAlbum(imageToCapture, nil, nil, nil);
-        UIGraphicsEndImageContext()
+        UIImageWriteToSavedPhotosAlbum(getImageView(), nil, nil, nil);
         
         downloadButton.backgroundColor = UIColor.whiteColor()
         UIView.animateWithDuration(NSTimeInterval(2.0), animations: {
             self.downloadButton.backgroundColor = UIColor.clearColor()
         })
+    }
+    
+    // Returns imageView's image + all annotations
+    private func getImageView() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.imageView.bounds.size, view.opaque, 0.0)
+        self.imageView.layer.renderInContext(UIGraphicsGetCurrentContext())
+        var imageCaptured = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return imageCaptured
     }
     
     // Toggle mode for annotation words
@@ -385,6 +300,145 @@ class NodeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
             oldImage.removeFromSuperview()
         })
     }
+    
+    // Adds an overlaying image from camera roll
+    @IBAction func addOverlayImage(sender: AnyObject){
+        if (editToggler == false) {
+            return
+        }
+        getImageFromPhotoLibrary(sender)
+        
+    }
+    
+    // Show/Hide all labels on annotations
+    @IBAction func swapAnnotationLabels(sender: AnyObject) {
+        if swappingToggler {
+            removeAllLabels()
+            swappingToggler = false
+            swapButton.layer.borderColor = UIColor.clearColor().CGColor
+        } else {
+            showAllLabels()
+            swappingToggler = true
+            swapButton.layer.borderColor = UIColor.whiteColor().CGColor
+        }
+    }
+    
+    private func removeAllLabels() {
+        for eachLabel in allLabels {
+            eachLabel.removeFromSuperview()
+        }
+    }
+    
+    private func showAllLabels() {
+        allLabels.removeAll(keepCapacity: false)
+        // Update association list
+        var roomRepresentation = mementoManager.getMemoryPalaceRoomView(graphName, roomLabel: roomLabel)!
+        associationList = roomRepresentation.associations
+        
+        // Load association list
+        for eachAssociation in associationList {
+            var placeHolderFrame = eachAssociation.placeHolder.view.frame
+            var placeHolderLabel = eachAssociation.placeHolder.label
+            self.showLabel(placeHolderFrame, labelNumber: placeHolderLabel)
+        }
+    }
+    
+    private func showLabel(annotatedFrame: CGRect, labelNumber: Int) {
+        var label = UILabel(frame: annotatedFrame)
+        label.text = String(labelNumber)
+        label.font = UIFont(name: "Helvetica", size: 50)
+        label.textColor = UIColor.whiteColor()
+        label.textAlignment = NSTextAlignment.Center
+        label.userInteractionEnabled = true
+        self.view.addSubview(label)
+        allLabels.append(label)
+    }
+    
+    
+    
+    // Helper functions for main photo pickers
+    private func getImageFromPhotoLibrary(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.SavedPhotosAlbum) {
+                var imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType =
+                    UIImagePickerControllerSourceType.PhotoLibrary
+                imagePicker.mediaTypes = [kUTTypeImage as NSString]
+                imagePicker.allowsEditing = false
+                
+                var popover = UIPopoverController(contentViewController: imagePicker) as UIPopoverController
+                var frame = CGRectMake(315, 260, 386, 386);
+                
+                popover.presentPopoverFromRect(frame, inView: self.view, permittedArrowDirections: UIPopoverArrowDirection.Any, animated: true)
+                
+                newMedia = false
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        let mediaType = info[UIImagePickerControllerMediaType] as NSString
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        if mediaType.isEqualToString(kUTTypeImage as NSString) {
+            var image = info[UIImagePickerControllerOriginalImage]
+                as UIImage
+            
+            // Adjustments to show UIImageView in proper rotation according to input types
+            if picker.sourceType == UIImagePickerControllerSourceType.Camera{
+                image = UIImage(CGImage: image.CGImage, scale:1, orientation: UIImageOrientation.Down)!
+            } else {
+                image = UIImage(CGImage: image.CGImage, scale:1, orientation: UIImageOrientation.Up)!
+            }
+            
+            // Adding draggable images
+            // Scaling factor for inserted images is default at height 150
+            var scalingFactor = image.size.height/150.0
+            var newWidth = image.size.width / scalingFactor
+            newImage = DraggableImageView(image: image)
+            newImage.graphName = self.graphName
+            newImage.roomLabel = self.roomLabel
+            newImage.frame = CGRect(x: imageView.center.x, y: imageView.center.y, width: newWidth, height: 150.0)
+            newImage.parentViewController = self
+            imageView.addSubview(newImage)
+            
+            // Get paths for saving
+            var newOverlay = mementoManager.addOverlay(graphName, roomLabel: roomLabel, frame: newImage.frame, image: (newImage.image!))!
+            newImage.labelIdentifier = newOverlay.label
+            
+            if (newMedia == true) {
+                // Option to save to camera roll/ Photo album
+                //UIImageWriteToSavedPhotosAlbum(image, self, "image:didFinishSavingWithError:contextInfo:", nil)
+            } else if mediaType.isEqualToString(kUTTypeMovie as NSString) {
+                // Code to support video here
+            }
+            
+        }
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafePointer<Void>) {
+        
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed",
+                message: "Failed to save image",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let cancelAction = UIAlertAction(title: "OK",
+                style: .Cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true,
+                completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /******************************************************************************************/
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if (segue.identifier == "CreateNewNodeSegue") {
