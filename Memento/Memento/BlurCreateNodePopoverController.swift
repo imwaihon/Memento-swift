a233f8e671b02aae2aa4eb167d1e78c7179e0995
jjingrong <jing_rong92@hotmail.com>
2015-04-21 18:08:08 +0800

    Updated BlurCreateNodeVC to use new methods

--- Memento/Memento/BlurCreateNodePopoverController.swift
+++ Memento/Memento/BlurCreateNodePopoverController.swift
@@ -139,6 +139,7 @@ class BlurCreateNodePopoverController: UIViewController, UIGestureRecognizerDele
                 } else {
                     self.parentVC?.imageView.image = image
                     mementoManager.setBackgroundImageForRoom(self.graphName, roomLabel: nextRoomLabel, newImage: Utilities.convertToScreenSize(image))
+                    mementoManager.setBackgroundImageForRoom(self.graphName, roomLabel: nextRoomLabel, newImage: Utilities.convertToScreenSize(image), imageType: Constants.ImageType.PNG)
                 }
                 self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
             }
@@ -153,7 +154,7 @@ class BlurCreateNodePopoverController: UIViewController, UIGestureRecognizerDele
             }
         } else {
             self.parentVC?.imageView.image = image
-            mementoManager.setBackgroundImageForRoom(self.graphName, roomLabel: nextRoomLabel, newImage: Utilities.convertToScreenSize(image))
+            mementoManager.setBackgroundImageForRoom(self.graphName, roomLabel: nextRoomLabel, newImage: Utilities.convertToScreenSize(image), imageType: Constants.ImageType.PNG)
         }
 
 
