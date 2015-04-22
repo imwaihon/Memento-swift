8c933c649e3b995cd96e4b764fe9262cfae456f1
jjingrong <jing_rong92@hotmail.com>
2015-04-22 15:18:49 +0800

    Updated new colors

--- Memento/Memento/Constants.swift
+++ Memento/Memento/Constants.swift
@@ -40,9 +40,9 @@ struct Constants {
     static let imgResourceDir = "sharedResource"
     
     // Color constants
-    static let color1 = "225378"
-    static let color2 = "1695A3"
-    static let color3 = "ACF0F2"
-    static let color4 = "FFFFFF"
-    static let color5 = "EB7F00"
+    static let color1 = "FF2D00"
+    static let color2 = "EB7F00"
+    static let color3 = "FFFFFF"
+    static let color4 = "A7C520"
+    static let color5 = "2980B9"
 }
