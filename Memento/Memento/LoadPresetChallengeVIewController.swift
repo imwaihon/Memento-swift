ee2d31003697b9640d12dcdbbc81c3263f20c5cb
imwaihon <imwaihon@gmail.com>
2015-04-24 18:18:50 +0800

    Removed extra file

--- Memento/Memento/LoadPresetChallengeVIewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LoadPresetChallengeVIewController.swift
-//  Memento
-//
-//  Created by Chee Wai Hon on 31/3/15.
-//  Copyright (c) 2015 NUS CS3217. All rights reserved.
-//
-
-import Foundation
