0c737b5d3792569a4b565af7875fb6bc437e7bcf
zixian92 <zixian1992@hotmail.com>
2015-04-25 15:52:44 +0800

    Documents methods in Placeholder classes

--- Memento/Memento/RectanglePlaceHolder.swift
+++ Memento/Memento/RectanglePlaceHolder.swift
@@ -30,8 +30,11 @@ class RectanglePlaceHolder: PlaceHolder {
         super.init(frame: highlightArea, color: color)
     }
     
-    //Decodes the plist representation into a RectanglePlaceHolder.
-    //rep should be the type of string that is returned by stringEncoding property.
+    /// Decodes the plist representation into a RectanglePlaceHolder.
+    /// rep should be the type of string that is returned by stringEncoding property.
+    ///
+    /// :param: rep The string representation of the placeholder object to be decoded.
+    /// :returns: The placeholder object represented by the given string encoding.
     override class func decodeFromString(rep: String) -> PlaceHolder {
         let arr = rep.componentsSeparatedByString(Constants.placeholderValueSeparator)
         return arr.count > 1 ? RectanglePlaceHolder(highlightArea: CGRectFromString(arr[0]), color: arr[1]): RectanglePlaceHolder(highlightArea: CGRectFromString(arr[0]))
